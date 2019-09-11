e93a007b52fdf6a087f5b5fe62582d98d36af60a
ethanrwilson1998 <43278339+ethanrwilson1998@users.noreply.github.com>
2019-09-11 10:52:49 -0400

    redid navigation within research

--- src/app/research-main/research-main.page.ts
+++ src/app/research-main/research-main.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular'
 
 @Component({
   selector: 'app-research-main',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResearchMainPage implements OnInit {
 
-  constructor() { }
+  constructor(public navCtrl: NavController) { }
 
   ngOnInit() {
+  }
+
+  goToLocomotionPage() {
+    this.navCtrl.navigateForward('/locomotion');
   }
 
 }
